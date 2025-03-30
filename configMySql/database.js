99327f1615fbca3b193e8b46b28a44856eba295b
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-03-31 00:02:42 +0200

    Test de la connection à la BD mysql OK!

--- configMySql/database.js
+++ configMySql/database.js
@@ -3,6 +3,9 @@ import mysql from 'mysql2'
 const pool = mysql.createPool({
     host:'127.0.0.1',
     user:'root',
-    password:'',
+    password:'2012b',
     database: 'hopital'
-}).promise()
+}).promise()  //promise pour avoir la possibilité d'utliser async
+
+const resultat = await pool.query("SELECT * FROM test")
+console.log(resultat)
