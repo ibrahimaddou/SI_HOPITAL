e09e97515698dc6aa50d37f1f014156af5b05e53
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-04-01 16:56:41 +0200

    ajout de la fonction getMedecinById - récupere un medecin avec son id à partir de la BD

--- configMySql/database.js
+++ configMySql/database.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const pool = mysql.createPool({
-    host:'127.0.0.1',
-    user:'root',
-    password:'2012b',
-    database: 'hopital'
+    host: process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }).promise()  //promise pour avoir la possibilité d'utliser async
 
 
@@ -12,5 +15,21 @@ async function getMedecins(){
     const [rows]= await pool.query("select personne.id_personne, personne.nom, personne.prenom , medecin.specialite from medecin, employe , personne where medecin.id_employe=employe.id_employe and employe.id_personne =personne.id_personne;")
     return rows;
 }
-const notes = await getMedecins();
-console.log(notes);
+
+async function getMedecinById(id){ // pour afficher les infos de medicin avec un son id 
+    const [rows]= await pool.query(`
+        select medecin.id_medecin,personne.id_personne, personne.nom, personne.prenom , medecin.specialite 
+        from medecin, employe , personne 
+        where medecin.id_medecin =?
+        and medecin.id_employe=employe.id_employe 
+        and employe.id_personne =personne.id_personne;`,[id]  
+         // eviter de l'injecter dans la requete avec  ${id} pour des raisons de security SQL injection
+    );
+    return rows;
+}
+
+const getmedecin = await getMedecinById(2); //exemple le medecin qui a l' id 2
+console.log(getmedecin);
+
+//const medecins = await getMedecins();
+//console.log(medecin);
