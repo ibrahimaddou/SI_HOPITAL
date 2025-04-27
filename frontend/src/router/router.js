4c992a53d54536648172de1c03f03f77bcb44c17
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-04-27 17:49:46 +0200

    ajout des Guards pour sécuriser les routes

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -8,7 +8,7 @@ import InfirmierDashboard from '@/components/InfirmierDashboard.vue'
 const routes = [
   {
     path: '/',
-    redirect: '/Acceuil'
+    redirect: '/acceuil'
   },
   {
     path: '/login',
@@ -18,17 +18,30 @@ const routes = [
   {
     path: '/admin/dashboard',
     name: 'Admin Dashboard',
-    component: AdminDashboard
+    component: AdminDashboard,
+    meta: {
+      requiresAuth: true,
+      role: 'admin'
+    }
+
   },
   {
     path: '/medecin/dashboard',
     name: 'Medecin Dashboard',
-    component: MedecinDashboard
+    component: MedecinDashboard,
+    meta: {
+      requiresAuth: true,
+      role: 'medecin'
+    }
   },
   {
     path: '/infirmier/dashboard',
     name: 'Infirmier Dashboard',
-    component: InfirmierDashboard
+    component: InfirmierDashboard,
+    meta: {
+      requiresAuth: true,
+      role: 'infirmier'
+    }
   },
 
 
@@ -38,6 +51,28 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// guard de navigation
+router.beforeEach(async (to, from) => {
+  //si l'utilisateur est authentifié
+  const isAuthenticated = localStorage.getItem('token') !== null
+  const userRole = localStorage.getItem('role')
+  
+  // si l'utilisateur n'est pas connecté
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    // redirect vers page accueil
+    return { path: '/accueil', query: { redirect: to.fullPath } }
+  }
+  
+  if (to.meta.role && to.meta.role !== userRole) {
+    // le cas ou l' utilisateur n'a pas le bon rôle
+      return { path: '/login' }
+   
+  }
+  
+  // si tout est OK
+  return true
 })
 
 export default router
